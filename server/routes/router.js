2e78492dddcb82d43abbddf7a609ed8ea7c6ecd4
sjoshi-1 <200455953@student.georgianc.on.ca>
2022-07-27 22:16:24 -0400

    router

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,11 +1,36 @@
 const express=require("express");
 const router = express.Router();
+const users= require("../models/userSchema");
 /*
 router.get("/",(req,res)=>{
     console.log("connect");
 });
 */
 
-router.post("/register",(req,res)=>
-console.log(req.body));
+router.post("/register",(req,res)=>{
+const{name,email,age,mobile,work,add,desc}=req.body; 
+if(!name ||!email ||!age || !mobile ||!work || !add || !desc){ 
+    res.status(404).send("please fill the data");
+}
+try{
+    const preuser= users.findOne({email:email});
+    console.log(preuser);
+    
+    if(preuser){
+        res.status(404).send("this user is already present");
+
+    }
+    else{
+        const adduser =new users({
+            name,email,age,mobile,work,add,desc
+        });
+         adduser.save();
+        res.status(201).json(adduser);
+        console.log(adduser);
+    }
+}
+catch(error){
+    res.status(404).send(error)
+}
+})
 module.exports = router;
