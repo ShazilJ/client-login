4ef2a52d3768451b7c0a8da2dc3e47d15d937971
sjoshi-1 <200455953@student.georgianc.on.ca>
2022-07-27 20:06:52 -0400

    add mongodb conn

---
+++ server/db/conn.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose");
+const DB="mongodb+srv://demo:<clientdemo>@cluster0.yp1lren.mongodb.net/client?retryWrites=true&w=majority";
+
+mongoose.connect(DB,{
+    
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>console.log("connection start")).catch((error)=>console.log(error.message));
