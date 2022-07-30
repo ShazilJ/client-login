4d496048128c3a0c35b8563404aa80598f0bf29e
nitin4679 <nitinjakhar1527@gmail.com>
2022-07-29 22:47:39 -0400

    Added bootstrap to the elements

--- client-login/src/components/Home.js
+++ client-login/src/components/Home.js
@@ -1,114 +1,128 @@
-import React, {useState, useEffect } from 'react'
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import { NavLink } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
-
-const [getuserdata,setUserdata]=useState([]);
-console.log(getuserdata);
-    const getdata= async(e)=>
-    {
-
-        const res = await fetch("/getdata",{
-        method:"GET" ,
-        headers:{
-            "Content-type":"application/json"
-        },
-        
+  const [getuserdata, setUserdata] = useState([]);
+  console.log(getuserdata);
+  const getdata = async (e) => {
+    const res = await fetch("/getdata", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
     });
 
-    const data=await res.json();
+    const data = await res.json();
     console.log(data);
 
-    if(res.status===422 || !data){
-    
-        console.log("error");
+    if (res.status === 422 || !data) {
+      console.log("error");
+    } else {
+      setUserdata(data);
+      console.log("get data");
     }
-    else{
-        setUserdata(data)
-        console.log("get data");
-    }
-    }   
+  };
 
-useEffect(()=>{
+  useEffect(() => {
     getdata();
-})
+  });
 
-//delete user
+  //delete user
 
-const deleteuser =async(id)=>{
-    const res2 = await fetch(`/deleteuser/${id}`,{
-        method:"DELETE" ,
-        headers:{
-            "Content-type":"application/json"
-        },
+  const deleteuser = async (id) => {
+    const res2 = await fetch(`/deleteuser/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
     });
 
     const deletedata = await res2.json();
     console.log(deletedata);
 
-    if(res2.status === 422 || !deletedata){
-        console.log("error");
+    if (res2.status === 422 || !deletedata) {
+      console.log("error");
+    } else {
+      console.log("user deleted");
+      getdata();
     }
-    else{
-        console.log("user deleted");
-        getdata();
-    }
-}
+  };
 
-    return (
-        <div className='mt-5'>
-            <div className='container'>
-                <div className='add_btn mt-2 mb-2'>
-                    <NavLink to="/register" className='btn btn-primary'> Add data</NavLink>
-                </div>
-                <table class="table">
-                    <thead>
-                        <tr className='table-dark'>
-                            <th scope="col">id</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Job</th>
-                            <th scope="col">Number</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            getuserdata.map((element,id)=>{
-                                return(
-                                    <>
-            <tr>
-                            <th scope="row">{id + 1}</th>
-                            <td>{element.name}</td>
-                            <td>{element.email}</td>
-                            <td>{element.work}</td>
-                            <td>{element.mobile}</td>
-                            <td className='d-flex justify-content-between'>
-                            <NavLink to={`view/${element._id}`}>   <button className='btn btn-success'><RemoveRedEyeIcon/></button></NavLink> 
+  return (
+    <>
+      <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> User Added Succesfully!
+        <button
+          type="button"
+          class="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="mt-5">
+        <div className="container">
+          <div className="add_btn mt-2 mb-2">
+            <NavLink to="/register" className="btn btn-primary">
+              {" "}
+              Add data
+            </NavLink>
+          </div>
+          <table class="table">
+            <thead>
+              <tr className="table-dark">
+                <th scope="col">id</th>
+                <th scope="col">Username</th>
+                <th scope="col">Email</th>
+                <th scope="col">Job</th>
+                <th scope="col">Number</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {getuserdata.map((element, id) => {
+                return (
+                  <>
+                    <tr>
+                      <th scope="row">{id + 1}</th>
+                      <td>{element.name}</td>
+                      <td>{element.email}</td>
+                      <td>{element.work}</td>
+                      <td>{element.mobile}</td>
+                      <td className="d-flex justify-content-between">
+                        <NavLink to={`view/${element._id}`}>
+                          {" "}
+                          <button className="btn btn-success">
+                            <RemoveRedEyeIcon />
+                          </button>
+                        </NavLink>
 
-                            <NavLink to ={`edit/${element._id}`}>
-                                <button className='btn btn-primary'><EditIcon/></button>
-                                </NavLink>
-                                <button className='btn btn-danger' onClick ={()=>deleteuser(element._id)}><DeleteOutlineIcon/></button>
-                            </td>
-                        </tr>                        
-                                    </>
-                                )
-                            })
-                        }
-                        
-                        
-                    </tbody>
-                </table>
-
-            </div>
-
+                        <NavLink to={`edit/${element._id}`}>
+                          <button className="btn btn-primary">
+                            <EditIcon />
+                          </button>
+                        </NavLink>
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => deleteuser(element._id)}
+                        >
+                          <DeleteOutlineIcon />
+                        </button>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
